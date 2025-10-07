52fe6a25dcfe2431c1e4f9883dee3acbc940a948
FlavioRoman <romanflavio3@hotmail.com>
2025-10-07 14:04:40 -0300

    feat: add middleware for logging and featuring actions in Redux store

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { pokemonsReducer } from "./reducers/pokemons";
-import { legacy_createStore as createStore } from "redux";
+import { featuring, logger } from "./middleware/index.js";
+import {
+  compose,
+  applyMiddleware,
+  legacy_createStore as createStore,
+} from "redux";
 
 import "@ant-design/v5-patch-for-react-19";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const store = createStore(pokemonsReducer);
+const composeEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger, featuring)
+);
+
+const store = createStore(pokemonsReducer, composeEnhancers);
 
 root.render(
   <React.StrictMode>
